8ab1e96df59c4d244e5862c2af8a4af9d2aac40d
danitoxqui <150280952+danitoxqui@users.noreply.github.com>
2023-11-09 22:09:10 -0500

    Initial commit

---
+++ 5/Videos-Login-main/src/app/models/usuario.model.ts
@@ -0,0 +1,19 @@
+
+
+export class UsuarioModel {
+
+  email: string;
+  password: string;
+  nombre: string;
+
+  constructor() {
+    this.email = ''; // Inicializar la propiedad "email" con un valor predeterminado
+    this.password = ''; // Inicializar la propiedad "password" con un valor predeterminado
+    this.nombre = ''; // Inicializar la propiedad "nombre" con un valor predeterminado
+  }
+
+
+
+
+
+}
