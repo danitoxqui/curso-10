8f60f1bf120559ed99534dea4b58dc1812b99474
danitoxqui <150280952+danitoxqui@users.noreply.github.com>
2023-11-10 22:21:23 -0500

    video 7

--- curso 10/src/app/pages/login/login.component.ts
+++ curso 10/src/app/pages/login/login.component.ts
@@ -21,10 +21,10 @@ usuario: UsuarioModel= new UsuarioModel();
 
     if ( form.invalid){ return; }
 
-    this.auth.login( this.usuario).subscribe( resp =>{
-      console.log(resp);
-    }, (err) =>{
-      console.log(err.error.error.message);
-    });
+    this.auth.login( this.usuario).subscribe( res =>{
+      console.log(res);
+    }, (er) =>{
+      console.log(er.error.error.message);
+    })
   }
 }
